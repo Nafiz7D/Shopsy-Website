4b4435ed7207670dfb52318dad450a17d7de200c
Nafiz7D <mrnafsu07@gmail.com>
2024-10-24 15:34:41 +0600

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image1 from "../../assets/hero/women.png";
+import Image2 from "../../assets/hero/shopping.png";
+import Image3 from "../../assets/hero/sale.png";
+
+const ImageList = [
+  {
+    id: 1,
+    img: Image1,
+    title: "Upto 50% off all Men's Wear",
+    description:
+      "Lorem His Life will forever be changed dolor sit amet, consecture adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    id: 2,
+    img: Image2,
+    title: "30% off on all Women's Wear",
+    description:
+      "Lorem His Life will forever be changed dolor sit amet, consecture adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    id: 1,
+    img: Image3,
+    title: "70% off all Products Sale",
+    description:
+      "Lorem His Life will forever be changed dolor sit amet, consecture adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+];
+
+const Hero = () => {
+  return <div>
+    {/*  */}
+    <div>
+
+    </div>
+  </div>;
+};
+
+export default Hero;
